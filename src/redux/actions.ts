90474aa3f47201c7836197d2d8c4654076fdc0d4
Nithesh <45152787+nk711@users.noreply.github.com>
2022-03-12 21:22:05 +0000

    Removed existing files

--- src/redux/actions.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const actions = {
-    add: (data: any) => ({ type: "ADD", payload: data}),
-    delete: (data: any) => ({ type: "DELETE", payload: data}),
-    filter: (data: any) => ({ type: "FILTER", payload: data}),
-}
