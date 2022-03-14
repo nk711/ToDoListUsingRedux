835ca276d5a165a9c4f681fc50a9ea4726747433
Nithesh <45152787+nk711@users.noreply.github.com>
2022-03-14 16:32:38 +0000

    Added and updated Card Stats component

--- src/components/CardStats.tsx
+++ src/components/CardStats.tsx
@@ -1,34 +1,42 @@
-// import { Picker } from "@react-native-picker/picker"
-// import React from "react"
-// import { StyleSheet, Text, View } from "react-native"
-// import { useRecoilState, useRecoilValue } from "recoil"
-// import { todoListFilterState, todoListStatsState } from "../recoil/store"
+import { Picker } from "@react-native-picker/picker"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { useSelector } from "react-redux"
+import { CardItem } from "../redux/cardsSlice"
 
-// export const CardStats = () => {
-
-//     const {
-//         total,
-//         totalCompleted,
-//         totalUncompleted,
-//         percentCompleted
-//     } = useRecoilValue(todoListStatsState)
-
-//     const formattedPercentCompleted = Math.round(percentCompleted)
+export const CardStats = () => {
     
-//     return ( 
-//         <View style={styles.stats}>
-//             <Text> * Total items: {total}</Text>
-//             <Text> * Items completed: {totalCompleted}</Text>
-//             <Text> * Items not completed: {totalUncompleted}</Text>
-//             <Text> * Percent completed: {formattedPercentCompleted}</Text>
-//         </View>
-//     )
-// }
+    const todoList = useSelector((state:any)=> {
+        switch (state.cards.filter) {
+            case 'Completed':
+                return state.cards.items.filter((item: CardItem) => item.isComplete) 
+            case 'InComplete':
+                return state.cards.items.filter((item: CardItem) => !item.isComplete) 
+            default:
+                return state.cards.items
+        }
+    })
+    
+    const total = todoList.length;
+    const totalCompleted = todoList.filter((item: CardItem) => item.isComplete).length;;
+    const totalIncomplete = todoList.filter((item: CardItem) => !item.isComplete).length;
+    const percentCompleted = total === 0 ? 0 : totalCompleted / total * 100;
+    const formattedPercentCompleted = Math.round(percentCompleted)
 
-// const styles = StyleSheet.create({
-//     stats: {
-//         fontWeight: '600',
-//         fontSize: 30,
-//         paddingVertical: 20,
-//     }
-// })
+    return ( 
+        <View style={styles.stats}>
+            <Text> * Total items: {total}</Text>
+            <Text> * Items completed: {totalCompleted}</Text>
+            <Text> * Items not completed: {totalIncomplete}</Text>
+            <Text> * Percent completed: {formattedPercentCompleted}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    stats: {
+        fontWeight: '600',
+        fontSize: 30,
+        paddingVertical: 20,
+    }
+})
