c9b738990467985ab9fba43d6b14a9040b523165
Nithesh <45152787+nk711@users.noreply.github.com>
2022-03-27 23:13:04 +0100

    Added Edit Card test. Encountering issue where Jest runs tests endlessly.

--- src/screens/__tests__/Home.test.js
+++ src/screens/__tests__/Home.test.js
@@ -1,7 +1,3 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import { Provider as StoreProvider } from 'react-redux';
@@ -54,86 +50,44 @@ describe('Testing Card Slices', () => {
     expect(toJSON()).toMatchSnapshot();
   });
 
-  test('can delete a  card', () => {
+  // test('can delete a  card', () => {
+  //   const {toJSON, getByTestId} = render(component);
+  //   const inputField = getByTestId('AddCard-Input');
+  //   const addCardButton = getByTestId('AddCard-Button');
+
+  //   fireEvent.changeText(inputField, 'Test Message');
+  //   fireEvent.press(addCardButton)
+
+  //   const list = store.getState().cards.items
+  //   const card = list[list.length - 1]
+
+  //   expect(card.key).toEqual(list.length)
+  //   expect(card.todo).toEqual('Test Message')
+  //   expect(card.isComplete).toEqual(false)
+
+  //   expect(toJSON()).toMatchSnapshot();
+  // });
+
+
+  test('can edit a card', () => {
     const {toJSON, getByTestId} = render(component);
     const inputField = getByTestId('AddCard-Input');
     const addCardButton = getByTestId('AddCard-Button');
-
     fireEvent.changeText(inputField, 'Test Message');
     fireEvent.press(addCardButton)
 
+    expect(card.todo).toEqual('Test Message')
+
+    const editInputField = getByTestId('Card-Input-1');
+    fireEvent.changeText(editInputField, 'Edited Message');
     const list = store.getState().cards.items
     const card = list[list.length - 1]
 
-    expect(card.key).toEqual(list.length)
-    expect(card.todo).toEqual('Test Message')
-    expect(card.isComplete).toEqual(false)
-
+    expect(card.todo).toEqual('Edited Message')
     expect(toJSON()).toMatchSnapshot();
   });
 
 
+
 });
 
-//  went to the beach
-// describe('Testing individual Card property', () => {
-
-//   test('Card values are displayed correctly', () => {
-//     const {getByTestId } = render(component);
-//     const inputField = getByTestId('Card.Input');
-//     const isCompleteButton = getByTestId('Card.IsComplete');
-
-
-//     expect(inputField.props.value).toBe('test message');
-//     expect(isCompleteButton.props.style.backgroundColor).toBe('orange');
-//   });
-
-//   test('Can set card to be completed', () => {
-//     const {getByTestId} = render(component);
-//     const inputField = getByTestId('Card.Input');
-//     const isCompleteButton = getByTestId('Card.IsComplete');
-//     fireEvent.press(isCompleteButton)
-
-//     expect(inputField.props.value).toBe('test message');
-//     expect(isCompleteButton.props.style.backgroundColor).toBe('#99d372');
-//     const card = store.getState().cards.filter((obj) => obj.key===item.key)
-//     expect(card.isComplete).toBe(true);
-//   });
-
-
-// });
-
-
-
-// describe('CardComponent', () => {
-
-
-  
-  
-//   test('If card is displayed properly', () => {
-//     store.dispatch(
-//       addCard({
-//           key: Math.random(),
-//           todo: 'test message :)',
-//           isComplete: false
-//       })
-//     );
-//     const {getByTestId } = render(component);
-//     const inputField = getByTestId('AddCard.Input');
-//     expect(inputField).toBeTruthy();
-    
-//   });
-
-
-//   test('if user can add a card.', () => {
-//     const {getByTestId} = render(component);
-//     const inputField = getByTestId('AddCard.Input');
-//     const addCardButton = getByTestId('AddCard.Button');
-//     fireEvent.changeText(inputField, 'Test Message');
-//     fireEvent.press(addCardButton)
-//     const list = store.getState().cards.items
-//     const card = list[list.length - 1]
-//     expect(card.todo).toEqual('Test Message');
-//     expect(card.isComplete).toEqual(false);
-//   });
-// });
