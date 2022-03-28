3754b4f7fd23ea14a6dcf0c4cdba1b5b43cbb61b
Nithesh <45152787+nk711@users.noreply.github.com>
2022-03-28 17:40:38 +0100

    Attempted to mock the picker module

--- test-config/setupJest.js
+++ test-config/setupJest.js
@@ -1,6 +1,19 @@
 import {jest} from '@jest/globals';
+import React, { Component } from 'react';
 require('jest-fetch-mock').enableMocks();
 
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+
+// jest.mock('@react-native-picker/picker', () => {
+//     (props) => {
+//         const MockComponent = require('./mockComponent')
+//         return <MockComponent 
+//             module='Picker' 
+//             onPress={props => props.disabled ? () => {} : props.onPress}
+//             {...props}
+//         />
+//    }
+// })
